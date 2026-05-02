76ee28bd4ff17daacf28e480e7904829daf950cf
Sumit Kumar Shaw <sumitshaw8587@gmail.com>
2026-05-02 11:56:50 +0530

    Initial Commit

---
+++ notification_app_fe/next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "@/RA2311030030049/notification_app_fe/node_modules/next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
